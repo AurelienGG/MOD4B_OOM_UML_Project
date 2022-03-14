b96ab93276272600c96bb92f6208e2c5814a65a5
EmmieKr <48325116+EmmieKr@users.noreply.github.com>
2022-03-14 12:04:32 -0400

    Update RunwayManager.java

--- application/logic/runways/RunwayManager.java
+++ application/logic/runways/RunwayManager.java
@@ -1,4 +1,15 @@
 package logic.runways;
 
 public class RunwayManager {
+    private static RunwayManager runwayManager_instance = null;
+    private Runway[] runwayDictionary;
+    private RunwayManager(){
+        Runway[] runwayDictionary =  new Runway[8];
+    }
+
+    public RunwayManager getInstance() {
+        if (runwayManager_instance == null)
+            runwayManager_instance = new RunwayManager();
+        return runwayManager_instance;
+    }
 }

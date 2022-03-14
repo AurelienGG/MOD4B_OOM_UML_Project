6daa02dd4e17adc392057b91c736a23cf4872bfd
EmmieKr <48325116+EmmieKr@users.noreply.github.com>
2022-03-14 11:10:52 -0400

    Update LockdownTheRunway.java

--- application/logic/options/LockdownTheRunway.java
+++ application/logic/options/LockdownTheRunway.java
@@ -1,4 +1,11 @@
 package logic.options;
 
 public class LockdownTheRunway extends Option {
+    private String description;
+    public boolean checkRequirement(){
+        return false;
+    }
+    public void applyConsequence(){
+
+    }
 }

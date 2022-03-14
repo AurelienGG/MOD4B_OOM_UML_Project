0466ed6b9df1233fbfb2a76391c815c3000cff71
AurelienGG <bh191036@ens.uqam.ca>
2022-03-14 09:48:03 -0400

    MAJ menu

--- application/userInterface/menus/Menu.java
+++ application/userInterface/menus/Menu.java
@@ -1,4 +1,12 @@
 package userInterface.menus;
 
 public abstract class Menu {
+
+    private MenuType menuType;
+
+    //public Menu() {}
+
+    public abstract void goToMenu(Menu menu);
+
+    public abstract String displayMenu();
 }

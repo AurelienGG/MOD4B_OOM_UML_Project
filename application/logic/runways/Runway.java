5ed54632a963632d535dda683ada2fb7b032daf1
EmmieKr <48325116+EmmieKr@users.noreply.github.com>
2022-03-14 11:06:23 -0400

    Update Runway.java

--- application/logic/runways/Runway.java
+++ application/logic/runways/Runway.java
@@ -1,4 +1,26 @@
 package logic.runways;
 
+import logic.planes.Plane;
+
 public class Runway {
+    private boolean occupied;
+    private Plane holderPlane;
+    public void planeLand(Plane plane){
+
+    }
+    public void decreasePlaneHoursRunwayNeeded(){
+
+    }
+    public String displayRunway() {
+        return "Runway{" +
+                "occupied=" + occupied +
+                ", holderPlane=" + holderPlane +
+                '}';
+    }
+    private void checkPlaneFuelLevel(){
+
+    }
+    private void planeLeave(){
+        
+    }
 }

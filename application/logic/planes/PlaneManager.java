9a51f08d559db7f5ec06254b4dbb516f5035c38f
EmmieKr <48325116+EmmieKr@users.noreply.github.com>
2022-03-14 11:48:27 -0400

    Update PlaneManager.java

--- application/logic/planes/PlaneManager.java
+++ application/logic/planes/PlaneManager.java
@@ -1,4 +1,20 @@
 package logic.planes;
 
+import java.util.EnumMap;
+import java.util.Stack;
+
 public class PlaneManager {
+    private static PlaneManager planeManager_instance = null;
+
+    private EnumMap<PlaneType, Stack<Plane>> planesDictionary; //voir pour le stack<Plane>
+
+    private PlaneManager(){
+        EnumMap<PlaneType, Stack<Plane>> planesDictionary =  new EnumMap<>(PlaneType.class);
+    }
+
+    public PlaneManager getInstance() {
+        if (planeManager_instance == null)
+            planeManager_instance = new PlaneManager();
+        return planeManager_instance;
+    }
 }

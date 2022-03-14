a4f742381624aae4b2b3e077df06cae02a02cee2
EmmieKr <48325116+EmmieKr@users.noreply.github.com>
2022-03-14 11:11:06 -0400

    Update LetTheManGo.java

--- application/logic/options/LetTheManGo.java
+++ application/logic/options/LetTheManGo.java
@@ -1,4 +1,11 @@
 package logic.options;
 
 public class LetTheManGo extends Option {
+    private String description;
+    public boolean checkRequirement(){
+        return false;
+    }
+    public void applyConsequence(){
+
+    }
 }

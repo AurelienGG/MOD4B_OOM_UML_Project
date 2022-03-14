36d1a8a9039d8164105b2e1a7bc7ecfc6f26d88c
EmmieKr <48325116+EmmieKr@users.noreply.github.com>
2022-03-14 11:09:18 -0400

    Update AirRefueling.java

--- application/logic/options/AirRefueling.java
+++ application/logic/options/AirRefueling.java
@@ -1,4 +1,11 @@
 package logic.options;
 
 public class AirRefueling extends Option {
+    private String description;
+    public boolean checkRequirement(){
+        return false;
+    }
+    public void applyConsequence(){
+
+    }
 }

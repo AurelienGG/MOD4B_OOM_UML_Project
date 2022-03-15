8cac87f61455be99c22810ec3310011781805ae8
EmmieKr <48325116+EmmieKr@users.noreply.github.com>
2022-03-15 12:12:33 -0400

    add super constructor request

--- application/logic/requests/JumboJet.java
+++ application/logic/requests/JumboJet.java
@@ -1,4 +1,10 @@
 package logic.requests;
 
+import logic.options.Option;
+import logic.planes.Plane;
+
 public class JumboJet extends Landing {
+    protected JumboJet(double RARITY, String TITLE, String DESCRIPTION, Option[] options, Plane plane) {
+        super(RARITY, TITLE, DESCRIPTION, options, plane);
+    }
 }

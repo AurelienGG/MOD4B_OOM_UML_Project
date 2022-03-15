e8d395a59daf374b7c53df60d25778a19d913530
AurelienGG <bh191036@ens.uqam.ca>
2022-03-14 21:43:54 -0400

    Initialise classes functions and added TODO package Request

--- application/logic/requests/Request.java
+++ application/logic/requests/Request.java
@@ -1,4 +1,19 @@
 package logic.requests;
 
+import logic.options.Option;
+
 public abstract class Request {
+
+    private final double RARITY;
+    private final String TITLE;
+    private final String DESCRIPTION;
+    private Option[] options;
+
+    protected Request(double RARITY, String TITLE, String DESCRIPTION, Option[] options) {
+        //TODO
+        this.RARITY = RARITY;
+        this.TITLE = TITLE;
+        this.DESCRIPTION = DESCRIPTION;
+        this.options = options;
+    }
 }
--- application/logic/waitingPlanes/WaitingPlanesManager.java
+++ application/logic/waitingPlanes/WaitingPlanesManager.java
@@ -1,4 +1,24 @@
 package logic.waitingPlanes;
 
+import logic.planes.Plane;
+import java.util.ArrayList;
+
 public class WaitingPlanesManager {
+
+    private static WaitingPlanesManager waitingPlanesManager_instance = null;
+    private ArrayList<Plane> waitingPlanes;
+
+    private WaitingPlanesManager() {
+        //TODO
+    }
+
+    public static WaitingPlanesManager getInstance() {
+        if (waitingPlanesManager_instance == null)
+            waitingPlanesManager_instance = new WaitingPlanesManager();
+        return waitingPlanesManager_instance;
+    }
+
+    public void addPlaneToWaitingPlanes(Plane plane) {
+        //TODO
+    }
 }

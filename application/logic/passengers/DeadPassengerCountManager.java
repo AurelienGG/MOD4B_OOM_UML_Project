1a0a6ead516b2c6e02e5facaa7f8eb253ce94b96
AurelienGG <bh191036@ens.uqam.ca>
2022-03-14 21:42:31 -0400

    Initialise classes functions and added TODO DeadPassengerCountManager.java

--- application/logic/passengers/DeadPassengerCountManager.java
+++ application/logic/passengers/DeadPassengerCountManager.java
@@ -1,4 +1,17 @@
 package logic.passengers;
 
 public class DeadPassengerCountManager {
+
+    private static DeadPassengerCountManager deadPassengerCountManager_instance = null;
+    private int nbDeadPassengers;
+
+    private DeadPassengerCountManager() {
+        //TODO
+    }
+
+    public static DeadPassengerCountManager getInstance() {
+        if (deadPassengerCountManager_instance == null)
+            deadPassengerCountManager_instance = new DeadPassengerCountManager();
+        return deadPassengerCountManager_instance;
+    }
 }

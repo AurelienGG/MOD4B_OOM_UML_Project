2d7ef46aa9107dc617f3f2d40d319d417e81c827
AurelienGG <bh191036@ens.uqam.ca>
2022-03-14 21:43:38 -0400

    Initialise classes functions and added TODO package SimulationHour

--- application/logic/simulationHour/SimulationHour.java
+++ application/logic/simulationHour/SimulationHour.java
@@ -1,4 +1,24 @@
 package logic.simulationHour;
 
+import logic.requests.Request;
+
+import java.util.ArrayList;
+
 public class SimulationHour {
+
+    private int currentHour;
+    private ArrayList<Request> requests;
+
+    public SimulationHour() {
+        //TODO
+    }
+
+    public void startRequest(Request request) {
+        //TODO
+    }
+
+    public String hourSummary() {
+        //TODO
+        return null;
+    }
 }

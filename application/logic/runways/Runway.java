ae177015597db1d94d7c9ce4f52a35d77b2e7514
AurelienGG <bh191036@ens.uqam.ca>
2022-03-14 21:43:18 -0400

    Initialise classes functions and added TODO package Runway

--- application/logic/runways/Runway.java
+++ application/logic/runways/Runway.java
@@ -3,24 +3,35 @@ package logic.runways;
 import logic.planes.Plane;
 
 public class Runway {
+
     private boolean occupied;
     private Plane holderPlane;
-    public void planeLand(Plane plane){
 
+    public Runway() {
+        //TODO
     }
-    public void decreasePlaneHoursRunwayNeeded(){
 
+    public void planeLand(Plane plane) {
+        //TODO
     }
+
+    public void decreasePlaneHoursRunwayNeeded() {
+        //TODO
+    }
+
     public String displayRunway() {
+        //TODO
         return "Runway{" +
                 "occupied=" + occupied +
                 ", holderPlane=" + holderPlane +
                 '}';
     }
-    private void checkPlaneFuelLevel(){
 
+    private void checkPlaneFuelLevel() {
+        //TODO
     }
-    private void planeLeave(){
-        
+
+    private void planeLeave() {
+        //TODO
     }
 }
--- application/logic/runways/RunwayManager.java
+++ application/logic/runways/RunwayManager.java
@@ -1,13 +1,15 @@
 package logic.runways;
 
 public class RunwayManager {
+
     private static RunwayManager runwayManager_instance = null;
-    private Runway[] runwayDictionary;
-    private RunwayManager(){
-        Runway[] runwayDictionary =  new Runway[8];
+    private Runway[] runways;
+
+    private RunwayManager() {
+        this.runways =  new Runway[8];
     }
 
-    public RunwayManager getInstance() {
+    public static RunwayManager getInstance() {
         if (runwayManager_instance == null)
             runwayManager_instance = new RunwayManager();
         return runwayManager_instance;

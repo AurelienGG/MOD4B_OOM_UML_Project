f8525bed11d84ad5e28551087c3929dcf9078f69
AurelienGG <bh191036@ens.uqam.ca>
2022-03-15 09:45:46 -0400

    add functions and javadoc WaitingPlanesManager.java

--- application/logic/waitingPlanes/WaitingPlanesManager.java
+++ application/logic/waitingPlanes/WaitingPlanesManager.java
@@ -24,7 +24,58 @@ public class WaitingPlanesManager {
         return waitingPlanesManager_instance;
     }
 
+    /**
+     * Add a plane to the waitingPlanes ArrayList
+     * @param plane the plane to add
+     */
     public void addPlaneToWaitingPlanes(Plane plane) {
         //TODO
     }
+
+    /**
+     * Hourly checks for waiting planes,
+     * Decrease the fuel,
+     * Check for crash
+     * Communicate number of dead passengers from crash
+     */
+    public void waitingPlanesAdvanceHour() {
+        //TODO
+    }
+
+    /**
+     * Decrease the fuel left in every waiting planes in the ArrayList
+     */
+    private void decreaseWaitingPlanesFuel() {
+        //TODO
+    }
+
+    /**
+     * Check the fuel left in every waiting planes in the ArrayList
+     * @return an ArrayList of planes out of fuel
+     * or null if there is no crash
+     */
+    private ArrayList<Plane> checkWaitingPlanesFuel() {
+        //TODO
+        return null;
+    }
+
+    /**
+     * Crash the waiting plane without fuels left
+     * @param crashingPlane the plane crashing
+     */
+    private void crashWaitingPlane(Plane crashingPlane) {
+        //TODO
+    }
+
+    /**
+     * The number of people dying from the crash
+     * @param crashingPlane the plane crashing
+     * @return the number of people dying from the crash
+     */
+    private int numberDeadPassengersFromCrash(Plane crashingPlane) {
+        //TODO
+        return 0;
+    }
+
+
 }

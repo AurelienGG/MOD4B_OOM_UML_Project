bfc1a9877541c28e430c28ae41005d50d7978760
AurelienGG <bh191036@ens.uqam.ca>
2022-03-16 12:10:54 -0400

    Added methods, javadoc and TODO simulation menu/ui package

--- application/userInterface/UserInputManager.java
+++ application/userInterface/UserInputManager.java
@@ -10,16 +10,38 @@ import java.util.Scanner;
 public class UserInputManager {
 
     private static UserInputManager ui_instance = null;
-
     private Scanner userInput;
 
+    /**
+     * TODO
+     */
     private UserInputManager() {
         this.userInput = new Scanner(System.in);
     }
 
+    /**
+     * TODO
+     * @return
+     */
     public static UserInputManager getInstance() {
         if (ui_instance == null)
             ui_instance = new UserInputManager();
         return ui_instance;
+    }
+
+    /**
+     * TODO
+     */
+    public void displayOptions() {
+        //TODO
+    }
+
+    /**
+     * TODO
+     * @return
+     */
+    public int readChosedOption() {
+        //TODO
+        return this.userInput.nextInt();
     }
 }
--- application/userInterface/menus/MainMenu.java
+++ application/userInterface/menus/MainMenu.java
@@ -1,22 +1,62 @@
 package userInterface.menus;
 
+import logic.passengers.DeadPassengerCountManager;
+import logic.simulationHour.SimulationHourManager;
+
 public class MainMenu extends Menu {
 
+    /**
+     * TODO
+     */
     public MainMenu() {
         super(MenuType.MAIN);
+        super.printMenuToOutput();
     }
 
+    /**
+     * TODO
+     */
     public void advanceHour() {
-
+        //TODO
     }
 
+    /**
+     * TODO
+     * @param menu
+     */
     @Override
     public void goToMenu(Menu menu) {
-
+        //TODO
     }
 
+    /**
+     * TODO
+     * @return
+     */
     @Override
     public String displayMenu() {
-        return "MainMenu";
+        //TODO
+        StringBuilder mainMenuDisplay = new StringBuilder();
+
+        mainMenuDisplay.append("MainMenu\n\n");
+
+        SimulationHourManager simulationHourManager = SimulationHourManager.getInstance();
+        mainMenuDisplay.append("\t\t\t\t\tcurrent hour " + simulationHourManager.getCurrentHour() + "h00" );
+
+        DeadPassengerCountManager deadPassengerCountManager = DeadPassengerCountManager.getInstance();
+        mainMenuDisplay.append("\t\t\t\t\t#passengers killed " + deadPassengerCountManager.getNbDeadPassengers() );
+
+        mainMenuDisplay.append("\nMenu options\n");
+
+
+        return mainMenuDisplay.toString();
+    }
+
+    /**
+     * TODO
+     * @return
+     */
+    public boolean checkVisibility() {
+        return false;
     }
 }

683b8a4d57fc32002d6e0d2aaadcff69d892470d
AurelienGG <bh191036@ens.uqam.ca>
2022-03-16 11:20:05 -0400

    Added methods, javadoc and TODO planes package

--- application/logic/planes/Plane.java
+++ application/logic/planes/Plane.java
@@ -14,6 +14,14 @@ public class Plane {
     private int hoursRunwayNeeded;
     private int nbPassengers;
 
+    /**
+     * TODO
+     * @param planeType
+     * @param name
+     * @param hoursFuelLeft
+     * @param hoursRunwayNeeded
+     * @param nbPassengers
+     */
     public Plane(PlaneType planeType, String name, int hoursFuelLeft, int hoursRunwayNeeded, int nbPassengers) {
         this.planeType=planeType;
         this.name=name;
@@ -22,34 +30,71 @@ public class Plane {
         this.nbPassengers=nbPassengers;
     }
 
+    /**
+     * TODO
+     * @return
+     */
     public PlaneType getPlaneType() {
         return planeType;
     }
 
+    /**
+     * TODO
+     * @return
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * TODO
+     * @return
+     */
     public int getHoursFuelLeft() {
         return hoursFuelLeft;
     }
 
+    /**
+     * TODO
+     * @return
+     */
     public int getHoursRunwayNeeded() {
         return hoursRunwayNeeded;
     }
 
+    /**
+     * TODO
+     * @return
+     */
     public int getNbPassengers() {
         return nbPassengers;
     }
 
+    /**
+     * TODO
+     */
     public void decreaseFuelLeft() {
         this.hoursFuelLeft--;
     }
 
+    /**
+     * TODO
+     */
     public void decreaseHoursRunwayNeeded() {
         this.hoursRunwayNeeded--;
     }
 
+    /**
+     * TODO
+     */
+    public void increaseHoursRunwayNeeded() {
+        this.hoursRunwayNeeded++;
+    }
+
+    /**
+     * TODO
+     * @return
+     */
     public String displayPlane() {
         //TODO
         return "Plane{" +

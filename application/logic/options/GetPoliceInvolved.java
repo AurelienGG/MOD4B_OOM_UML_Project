a3bc85d577e9ab5c6d01170a97ef59c813b62899
EmmieKr <48325116+EmmieKr@users.noreply.github.com>
2022-03-16 16:07:48 -0400

    Update GetPoliceInvolved.java

--- application/logic/options/GetPoliceInvolved.java
+++ application/logic/options/GetPoliceInvolved.java
@@ -1,16 +1,23 @@
 package logic.options;
 
+import logic.runways.RunwayManager;
+
 public class GetPoliceInvolved extends Option {
 
     private final String description = "Get police involved";
+    RunwayManager runwayManager;
 
     public GetPoliceInvolved(){
         super("Get police involved");
+        runwayManager=RunwayManager.getInstance();
     }
 
     @Override
     public boolean checkRequirement() {
-        return false;
+        if(runwayManager.getNbFreeRunway()>=2)
+            return true;
+        else
+            return false;
     }
 
     @Override

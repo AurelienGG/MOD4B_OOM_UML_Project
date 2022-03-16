4eb0879cfd95ddb2ea0e4422501f1993b391be9c
EmmieKr <48325116+EmmieKr@users.noreply.github.com>
2022-03-16 16:00:41 -0400

    requirement alllowlanding

--- application/logic/options/AllowLanding.java
+++ application/logic/options/AllowLanding.java
@@ -1,16 +1,22 @@
 package logic.options;
 
+import logic.runways.RunwayManager;
+
 public class AllowLanding extends Option {
 
+    RunwayManager runwayManager;
+
     public AllowLanding() {
-        //TODO
         super("Allow landing");
+        runwayManager = RunwayManager.getInstance();
     }
 
     @Override
     public boolean checkRequirement() {
-        //TODO
-        return false;
+        if(runwayManager.getNbFreeRunway()>0)
+            return true;
+        else
+            return false;
     }
 
     @Override

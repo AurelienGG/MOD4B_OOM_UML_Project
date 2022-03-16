901c72bc24f802094867a1c6974da5f058b70140
EmmieKr <48325116+EmmieKr@users.noreply.github.com>
2022-03-16 16:30:18 -0400

    Update DeadPassengerCountManager.java

--- application/logic/passengers/DeadPassengerCountManager.java
+++ application/logic/passengers/DeadPassengerCountManager.java
@@ -11,16 +11,27 @@ public class DeadPassengerCountManager {
     private static DeadPassengerCountManager deadPassengerCountManager_instance = null;
     private int nbDeadPassengers;
 
+    /**
+     * Constructor of Dead Passenger Count Manager
+     */
     private DeadPassengerCountManager() {
         this.nbDeadPassengers = 0;
     }
 
+    /**
+     * get the singleton of the instance of the Dead Passenger Count Manager
+     * @return deadPassengerCountManager_instance the singleton of the instance of the Dead Passenger Count Manager
+     */
     public static DeadPassengerCountManager getInstance() {
         if (deadPassengerCountManager_instance == null)
             deadPassengerCountManager_instance = new DeadPassengerCountManager();
         return deadPassengerCountManager_instance;
     }
 
+    /**
+     * Get the number of dead passengers
+     * @return the number of dead passengers
+     */
     public int getNbDeadPassengers() {
         return nbDeadPassengers;
     }

82fd94e46292efdb3a29b25a0ac6644d2fea7499
AurelienGG <bh191036@ens.uqam.ca>
2022-03-22 18:52:02 -0400

    Add HourManager, need a bit of refactoring

---
+++ application/logic/hour/HourManager.java
@@ -0,0 +1,43 @@
+package logic.hour;
+
+/**
+ * TODO
+ */
+public class HourManager {
+
+    private static HourManager hourManager_instance = null;
+    private int hour;
+
+    /**
+     * TODO
+     */
+    private HourManager() {
+        this.hour = 0;
+    }
+
+    /**
+     * TODO
+     * @return
+     */
+    public static HourManager getInstance() {
+        if (hourManager_instance == null)
+            hourManager_instance = new HourManager();
+        return hourManager_instance;
+    }
+
+    /**
+     * TODO
+     * @return
+     */
+    public int getHour() {
+        return this.hour;
+    }
+
+    /**
+     * TODO
+     * @return
+     */
+    public String displayHour() {
+        return this.hour + "h00";
+    }
+}
